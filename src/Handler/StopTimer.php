9e709e9104851b597f5730692be2aae6fc4a6c25
Scrutinizer Auto-Fixer <auto-fixer@scrutinizer-ci.com>
2017-09-26 12:16:57 +0000

    Scrutinizer Auto-Fixes
    This commit consists of patches automatically generated for this project on https://scrutinizer-ci.com

--- src/Handler/StopTimer.php
+++ src/Handler/StopTimer.php
@@ -59,7 +59,7 @@ class StopTimer
      */
     private function onSuccess(RequestInterface $request)
     {
-        return function (ResponseInterface $response) use ($request) {
+        return function(ResponseInterface $response) use ($request) {
             $this->responseTimeLogger->stop($request, $response);
         };
     }
@@ -71,7 +71,7 @@ class StopTimer
      */
     private function onFailure(RequestInterface $request)
     {
-        return function (ResponseInterface $response) use ($request) {
+        return function(ResponseInterface $response) use ($request) {
             $this->responseTimeLogger->stop($request, $response);
         };
     }
